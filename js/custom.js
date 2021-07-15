6dcf791da50e5dc2493e146423e8c4650790c2e5
deanoamor <toonoob4@gmail.com>
2021-07-15 18:36:35 +0700

    first commit

---
+++ js/custom.js
@@ -0,0 +1,108 @@
+
+  $(document).ready(function(){
+
+    $(".pic1").hover(function(){
+      $(".gambar").css("background-image", "url(/images/cursor-07.png)");
+      }, function(){
+        $(".gambar").css("background-image", "url(/images/cursor-01.png)");
+    });
+
+    $(".pic2").hover(function(){
+      $(".gambar").css("background-image", "url(/images/cursor-03.png)");
+      }, function(){
+        $(".gambar").css("background-image", "url(/images/cursor-01.png)");
+    });
+
+    $(".pic3").hover(function(){
+      $(".gambar").css("background-image", "url(/images/cursor-06.png)");
+      }, function(){
+        $(".gambar").css("background-image", "url(/images/cursor-01.png)");
+    });
+
+    $(".pic4").hover(function(){
+      $(".gambar").css("background-image", "url(/images/cursor-05.png)");
+      }, function(){
+        $(".gambar").css("background-image", "url(/images/cursor-01.png)");
+    });
+
+    $(".pic5").hover(function(){
+      $(".gambar").css("background-image", "url(/images/cursor-04.png)");
+      }, function(){
+        $(".gambar").css("background-image", "url(/images/cursor-01.png)");
+    });
+
+    $(".pic6").hover(function(){
+      $(".gambar").css("background-image", "url(/images/cursor-08.png)");
+      }, function(){
+        $(".gambar").css("background-image", "url(/images/cursor-01.png)");
+    });
+
+    $(".pic7").hover(function(){
+      $(".gambar").css("background-image", "url(/images/cursor-02.png)");
+      }, function(){
+        $(".gambar").css("background-image", "url(/images/cursor-01.png)");
+    }); 
+    
+    $(".pic8").hover(function(){
+      $(".gambar").css("background-image", "url(/images/cursor-09.png)");
+      }, function(){
+        $(".gambar").css("background-image", "url(/images/cursor-01.png)");
+    });
+
+
+    // $(".pic1").mouseover(function () {
+    //   $(".gambar").css("background-image", "url(/images/cursor-07.png)");
+    // });
+    // $(".pic1").mouseout(function () {
+    //   $(".gambar").css("background-image", "url(/images/cursor-01.png)");
+    // });
+
+    // $(".pic2").mouseover(function () {
+    //   $(".gambar").css("background-image", "url(/images/cursor-03.png)");
+    // });
+    // $(".pic2").mouseout(function () {
+    //   $(".gambar").css("background-image", "url(/images/cursor-01.png)");
+    // });
+
+    // $(".pic3").mouseover(function () {
+    //   $(".gambar").css("background-image", "url(/images/cursor-06.png)");
+    // });
+    // $(".pic3").mouseout(function () {
+    //   $(".gambar").css("background-image", "url(/images/cursor-01.png)");
+    // });
+
+    // $(".pic4").mouseover(function () {
+    //   $(".gambar").css("background-image", "url(/images/cursor-05.png)");
+    // });
+    // $(".pic4").mouseout(function () {
+    //   $(".gambar").css("background-image", "url(/images/cursor-01.png)");
+    // });
+
+    // $(".pic5").mouseover(function () {
+    //   $(".gambar").css("background-image", "url(/images/cursor-04.png)");
+    // });
+    // $(".pic5").mouseout(function () {
+    //   $(".gambar").css("background-image", "url(/images/cursor-01.png)");
+    // });
+
+    // $(".pic6").mouseover(function () {
+    //   $(".gambar").css("background-image", "url(/images/cursor-08.png)");
+    // });
+    // $(".pic6").mouseout(function () {
+    //   $(".gambar").css("background-image", "url(/images/cursor-01.png)");
+    // });
+
+    // $(".pic7").mouseover(function () {
+    //   $(".gambar").css("background-image", "url(/images/cursor-02.png)");
+    // });
+    // $(".pic7").mouseout(function () {
+    //   $(".gambar").css("background-image", "url(/images/cursor-01.png)");
+    // });
+
+    // $(".pic8").mouseover(function () {
+    //   $(".gambar").css("background-image", "url(/images/cursor-09.png)");
+    // });
+    // $(".pic8").mouseout(function () {
+    //   $(".gambar").css("background-image", "url(/images/cursor-01.png)");
+    // });
+  });
